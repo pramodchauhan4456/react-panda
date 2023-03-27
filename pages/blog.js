d1817e97664a81420bf4050267ecbcff30152498
pramodchauhanting <108465153+pramodchauhanting@users.noreply.github.com>
2023-03-27 14:26:05 +0530

    changes done

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,32 @@
 import React from "react";
-
+import Link from "next/link";
+import styles from "../styles/Blog.module.css";
 const Blog = () => {
-  return <div>blog</div>;
+  return (
+    <main className={styles.main}>
+      <div className="container">
+        <div className={styles.blogItem}>
+          <Link href="/blogpost/learn-javascript">
+            <h3>How to learn Javascript </h3>
+          </Link>
+
+          <p>Javascript is the language used to design for the web</p>
+        </div>
+        <div className={styles.blogItem}>
+          <h3>How to learn Javascript </h3>
+          <p>Javascript is the language used to design for the web</p>
+        </div>
+        <div className={styles.blogItem}>
+          <h3>How to learn Javascript </h3>
+          <p>Javascript is the language used to design for the web</p>
+        </div>
+        <div className={styles.blogItem}>
+          <h3>How to learn Javascript </h3>
+          <p>Javascript is the language used to design for the web</p>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Blog;
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,10 +1,24 @@
 import React from "react";
 import { useRouter } from "next/router";
+import styles from "../../styles/BlogPost.module.css";
 
 const slug = () => {
   const router = useRouter();
   const { slug } = router.query;
-  return <div>{slug}</div>;
+  return (
+    <div className="container">
+      <main className={styles.main}>
+        <h1>Title of the page{slug}</h1>
+        <hr />
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default slug;
